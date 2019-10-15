94a8787ed1eb62d9ea80fd20bf11335ed85d8a5c
Abhishek Kumar Singh <abhishek291093@gmail.com>
2019-10-15 21:08:38 +0530

    AKS | added werkzeug | added requirements.txt

--- twittit_backend/settings.py
+++ twittit_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pcn5z45%f5&xomq64010_+qfnq&-s$3@&onjjym$dg1_jrg+9i'
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
