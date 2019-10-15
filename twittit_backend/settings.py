a735725364173d5839b1607eb09625e2d0a504d4
Abhishek Kumar Singh <abhishek291093@gmail.com>
2019-10-15 21:52:50 +0530

    AKS | added twittit app

--- twittit_backend/settings.py
+++ twittit_backend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions'
+    'django_extensions',
+    'twittit.apps.TwittitConfig',
 ]
 
 MIDDLEWARE = [
